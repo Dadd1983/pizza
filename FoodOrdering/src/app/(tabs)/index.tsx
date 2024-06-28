fb9fb5424b6d840463cd54185ba7256ee8900fe0
Dadd1983 <powerpropaintingllc@gmail.com>
2024-06-27 21:39:21 -0400

    Removed .idea added main component

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,31 +1,34 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { StyleSheet, Image } from 'react-native';
+import  products  from '../../../assets/data/products'
 import { Text, View } from '@/src/components/Themed';
 
+const product = products[1]
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>hel</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+     <Image source={{ uri: product.image }} style={{ width: 200, height: 200 }} />
+     <Text style={styles.title}>{product.name}</Text>
+     <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
+  price: {
+    fontSize: 35,
+    fontWeight: 800,
+  }
 });
